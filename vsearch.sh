497adf47596d2fde3e35210152ea25fc75426317
huangyingw@linux_programming <huangyingw@gmail.com>
2015-01-06 11:52:03 -0800

    n

--- vsearch.sh
+++ vsearch.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-echo path in "$1/"
-echo keyword is "$2"
 find_result="$1/""`echo "$2".csc.findresult |sed  -e "s/\//\_/g;s/\ /\_/g"`"
-echo find_result is "$find_result"
-cscope -dL -f "$1/"cscope.out -4"$2" > "$find_result" 
+if [ -f "$find_result" ]; then
+  read -p "the search is already done, if you want to update, press u --> " update
+  case $update in
+    u)
+      ;;
+    "")
+      exit 1
+      ;;
+    ?)
+      exit 1
+      ;;
+  esac
+fi
+fgrep -wnH "$2" `cscope -dL -f "$1/"cscope.out -4"$2"|awk '{print $1}'|awk '!x[$0]++'` > "$find_result"  
