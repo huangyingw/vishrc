bffd35ab1cf935e4e294f11442548d1515e5187f
huangyingw@linux_programming <huangyingw@gmail.com>
2015-03-13 10:58:59 -0700

    n

--- vaa.sh
+++ vaa.sh
@@ -1,6 +1,39 @@
 #!/bin/bash
-#upsearch () {
-#  test / == "$PWD" && return || test -e "$1" && echo "found: " "$PWD" && return || cd .. && upsearch "$1"
-#}
-#upsearch "cscope.out"
-~/bashrc/faa.sh "$1" "$2"
+PRUNE_POSTFIX="$1/"prunefix.findresult
+PRUNE_FILE="$1/"prunefile.findresult
+prune_params=();
+prune_files=();
+find_result="$1/""`echo "$2".vaa.findresult |sed  -e "s/\//\_/g;s/\ /\_/g"`"
+if [ -f "$find_result" ]; then
+  read -p "the search is already done, if you want to update, press u --> " update
+  case $update in
+    u)
+      ;;
+    "")
+      exit 1
+      ;;
+    ?)
+      exit 1
+      ;;
+  esac
+fi
+or="";
+grep_params="";
+if [ -n "$3" ]
+then grep_params=" -A"$3" -B"$3;
+fi
+or="";
+while read suf
+do
+  prune_params+=( $or "-iname" "*.$suf" )
+  or="-o"
+done < "$PRUNE_POSTFIX"
+while read suf
+do
+  prune_files+=( $or "-wholename" "$suf" )
+  or="-o"
+done < "$PRUNE_FILE"
+echo "search in " "$1"
+cd "$1"
+echo "result in " "$find_result"
+find "$1" "(" "${prune_params[@]}" "${prune_files[@]}" "-o" "-name" "$find_result" ")" -prune -o -type f -exec fgrep -wnH  $grep_params "$2" {} \; > "$find_result"
