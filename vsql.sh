a5a6f438174225e68d480356c8d07bc975adf680
huangyingw@linux_programming <huangyingw@gmail.com>
2015-04-13 15:40:34 -0700

    n

--- vsql.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-file=${1}
-user=`cat ~/loadrc/vishrc/.login |awk -F'=' '/user/{print $2}'`
-password=`cat ~/loadrc/vishrc/.login |awk -F'=' '/password/{print $2}'`
-host=`cat ~/loadrc/vishrc/.login |awk -F'=' '/host/{print $2}'`
-dbinstance=`cat ~/loadrc/vishrc/.login |awk -F'=' '/dbinstance/{print $2}'`
-mysql -u"$user" -p"$password" -h${host} ${dbinstance} < ${file}
